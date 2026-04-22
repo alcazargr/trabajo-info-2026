a1f6280fcbc34cf1fb402f62058d3a26b4565519
lucaaksel-lang <lucaaksel@gmail.com>
2026-04-22 11:28:47 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#define N 10000
+
+
+typedef struct { //Estructura principal del archivo, dividida en subestructuras.
+    struct {
+        int anio;
+        int mes;
+        int dia;
+        char dia_semana [10];
+    } fecha;
+
+
+    struct {
+        char hora_ini[6];
+        char hora_fin[6];
+    } horario;
+
+
+    struct {
+        char  actividad_base [50];
+        char modalidad  [50];
+        char centro [50];
+        char tipo_uso  [50];
+    } actividad;
+
+
+    struct {
+        int plazas;
+        int ocupadas;
+        int libres;
+    } aforo;
+} Registro;
+
+
+int main ()
+{
+//Vector de estructuras:
+Registro vector_informacion [N];
+
+int i = 1; // para probar la lectura del archivo
+
+
+
+
+
+
+FILE*ARCHIVO; //Creacion del puntero del archivo y lectura
+ARCHIVO = fopen("deportes_ayuntamiento.txt" , "r");
+fscanf(ARCHIVO, "%*[^\n]\n"); //Se salta la linea de los headers.
+
+
+fscanf(ARCHIVO, "%d %d %d %s %s %s %s %s %s %d %d %d %s" , &vector_informacion[i].fecha.anio , &vector_informacion[i].fecha.mes, &vector_informacion[i].fecha.dia, vector_informacion[i].fecha.dia_semana, vector_informacion[i].horario.hora_ini, vector_informacion[i].horario.hora_fin, vector_informacion[i].actividad.actividad_base , vector_informacion[i].actividad.modalidad, vector_informacion[i].actividad.centro, &vector_informacion[i].aforo.plazas, &vector_informacion[i].aforo.ocupadas, &vector_informacion[i].aforo.libres, vector_informacion[i].actividad.tipo_uso);
+
+if (ARCHIVO == NULL) {
+        printf("ERROR: No se ha podido abrir el archivo. Comprueba el nombre y la carpeta.\n");
+}
+
+
+printf("%d %d %d %s %s %s %s %s %s %d %d %d %s" , vector_informacion[i].fecha.anio , vector_informacion[i].fecha.mes, vector_informacion[i].fecha.dia, vector_informacion[i].fecha.dia_semana, vector_informacion[i].horario.hora_ini, vector_informacion[i].horario.hora_fin, vector_informacion[i].actividad.actividad_base , vector_informacion[i].actividad.modalidad, vector_informacion[i].actividad.centro, vector_informacion[i].aforo.plazas, vector_informacion[i].aforo.ocupadas, vector_informacion[i].aforo.libres, vector_informacion[i].actividad.tipo_uso);
+
+
+
+
+return 0;
+}
